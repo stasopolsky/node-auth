dec1bba8ecf09a4bb89f22a7f26f62aaedd3fca3
Stas Opolsky <stas.opolsky@gmail.com>
2024-03-03 16:43:00 +0200

    auth service/controller

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,14 +1,17 @@
 const UserModel = require("../models/user-model");
 const bcrypt = require("bcrypt");
+const uuid = require("uuid");
 const mailService = require("./mail-service");
 const tokenService = require("./token-service");
-const userDto = require("../dtos/user-dto");
+const UserDto = require("../dtos/user-dto.js");
+
+const ApiError = require("../exceptions/api-error.js");
 
 class UserService {
   async registration(email, password) {
     const candidate = await UserModel.findOne({ email });
     if (candidate) {
-      throw new Error(`User with ${email} already exist`);
+      throw ApiError.BadRequest(`User with ${email} already exist`);
     }
     const hashPassword = await bcrypt.hash(password, 3);
     const activationLink = uuid.v4();
@@ -19,9 +22,12 @@ class UserService {
       activationLink,
     });
 
-    await mailService.sendActivationMail(email, activationLink);
+    await mailService.sendActivationMail(
+      email,
+      `${process.env.API_URL}/api/activate/${activationLink}`
+    );
 
-    const UserDto = new UserDto(user);
+    const userDto = new UserDto(user); // id, email, isActivated
     const tokens = tokenService.generateTokens({ ...userDto });
 
     await tokenService.saveToken(userDto.id, tokens.refreshToken);
@@ -30,6 +36,58 @@ class UserService {
       ...tokens,
       user: userDto,
     };
+  }
+
+  async activate(activationLink) {
+    const user = await UserModel.findOne({ activationLink });
+    if (!user) {
+      throw ApiError.BadRequest("Activation link not correct");
+    }
+    user.isActivated = true;
+    await user.save();
+  }
+
+  async login(email, password) {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      throw ApiError.BadRequest("Пользователь с таким email не найден");
+    }
+    const isPassEquals = await bcrypt.compare(password, user.password);
+    if (!isPassEquals) {
+      throw ApiError.BadRequest("Неверный пароль");
+    }
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return { ...tokens, user: userDto };
+  }
+
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken);
+    return token;
+  }
+
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiError.UnauthorizedError();
+    }
+    const userData = tokenService.validateRefreshToken(refreshToken);
+    const tokenFromDb = await tokenService.findToken(refreshToken);
+    if (!userData || !tokenFromDb) {
+      throw ApiError.UnauthorizedError();
+    }
+    const user = await UserModel.findById(userData.id);
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return { ...tokens, user: userDto };
+  }
+
+  async getAllUsers() {
+    const users = await UserModel.find();
+    return users;
   }
 }
 
